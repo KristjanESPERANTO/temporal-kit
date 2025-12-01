ad47b87db0bc444fc386045120f409bec50be9f6
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-12-01 14:07:42 +0100

    refactor(release): convert update-html-version script from CJS to ESM
    Package is configured with 'type: module', so scripts should use ESM too.

--- scripts/update-html-version.js
+++ scripts/update-html-version.js
@@ -1,7 +1,7 @@
-module.exports.readVersion = (contents) => {
+export const readVersion = (contents) => {
   const match = contents.match(/temporal-kit@([0-9]+\.[0-9]+\.[0-9]+)/);
   return match ? match[1] : null;
 };
 
-module.exports.writeVersion = (contents, version) =>
+export const writeVersion = (contents, version) =>
   contents.replace(/temporal-kit@[0-9]+\.[0-9]+\.[0-9]+/g, `temporal-kit@${version}`);
